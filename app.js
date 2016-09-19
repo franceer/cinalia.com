75960424f4dbeea5f82b0123b1ad0ca538408947
Erwin France <erwinfrance@gmail.com>
2016-09-19 17:16:05 +0200

    First save Cinalia.com

---
+++ app.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+
+//Set Modules & variables
+let express = require('express')
+    , app = express()
+    , router = express.Router();
+
+
+//App configuration
+let port = process.env.PORT || 80;
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+router.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.use(router);
+
+//Listen to port
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
